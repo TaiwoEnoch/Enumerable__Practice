c0f546eac34ec1704e20f001d93f4ca4364716d3
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-05-26 14:31:56 +0100

    Add my_list as a separate file to initialize list

---
+++ my_list.rb
@@ -0,0 +1,12 @@
+require_relative 'my_enumerable'
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    @list.each { |item| yield item }
+  end
+end
